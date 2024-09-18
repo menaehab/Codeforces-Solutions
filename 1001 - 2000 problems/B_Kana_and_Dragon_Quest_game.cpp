14d2203278073404e9664a484237dff615a9881e
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-09-18 12:41:25 +0300

    9/18/2024 - add 3 problems

---
+++ 1001 - 2000 problems/B_Kana_and_Dragon_Quest_game.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long
+#define el '\n'
+#define all(arr) arr.begin(), arr.end()
+#define allr(arr) arr.rbegin(), arr.rend()
+#define pi 3.14159265358979323846
+using namespace std;
+void setup()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+    setup();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, n, m;
+        cin >> x >> n >> m;
+        while (n > 0 && x > (x / 2 + 10))
+        {
+            x = x / 2 + 10;
+            n--;
+        }
+        while (m > 0 && x > 0)
+        {
+            x -= 10;
+            m--;
+        }
+        if (x <= 0)
+            cout << "YES" << el;
+        else
+            cout << "NO" << el;
+    }
+
+    return 0;
+}
