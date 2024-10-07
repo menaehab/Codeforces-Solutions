327d2039de0ba206c2e3ac54264b3431e62f088b
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-10-07 10:08:40 +0300

    10/6/2024 - add 3 problems

---
+++ 1001 - 2000 problems/I_Smallest_Pair.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long
+#define el '\n'
+#define all(arr) arr.begin(), arr.end()
+#define allr(arr) arr.rbegin(), arr.rend()
+#define pi 3.14159265358979323846
+using namespace std;
+void setup()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+    setup();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, mn = INT_MAX;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (i != j)
+                {
+                    mn = min(mn, arr[i] + arr[j] + j - i);
+                }
+            }
+        }
+        cout << mn << el;
+    }
+    return 0;
+}
