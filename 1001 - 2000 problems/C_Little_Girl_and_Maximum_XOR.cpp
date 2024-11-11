858ed0b86150fdcde95a516df4620560dc35206a
Mena Ehab <commanderyt123@gmail.com>
2024-11-11 09:15:12 +0200

    11/10/2024 - add 3 problems

---
+++ 1001 - 2000 problems/C_Little_Girl_and_Maximum_XOR.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long
+#define el '\n'
+#define all(arr) arr.begin(), arr.end()
+#define allr(arr) arr.rbegin(), arr.rend()
+#define pi 3.14159265358979323846
+using namespace std;
+void setup()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+  setup();
+  ll l, r;
+  cin >> l >> r;
+  ll p = l ^ r;
+  ll x = 1;
+  while (x <= p)
+  {
+    x = x << 1;
+  }
+  cout << x - 1;
+  return 0;
+}
