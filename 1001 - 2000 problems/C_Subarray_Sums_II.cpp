ed9cf649104039aab9256179d4bcf6bf0233bd01
Mena Ehab <commanderyt123@gmail.com>
2024-11-11 12:30:10 +0200

    11/11/2024 - add 3 problems

---
+++ 1001 - 2000 problems/C_Subarray_Sums_II.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long
+#define el '\n'
+#define all(arr) arr.begin(), arr.end()
+#define allr(arr) arr.rbegin(), arr.rend()
+#define pi 3.14159265358979323846
+using namespace std;
+void setup()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+  setup();
+  ll n, k, sum = 0, ans = 0;
+  cin >> n >> k;
+  vector<int> arr(n);
+  map<ll, ll> mp;
+  mp[sum] = 1;
+  for (int &x : arr)
+  {
+    cin >> x;
+    sum += x;
+    if (mp[sum - x])
+    {
+      ans += mp[sum - k];
+    }
+    mp[sum]++;
+  }
+  cout << ans << el;
+
+  return 0;
+}
---
+++ 1001 - 2000 problems/G_Kefa_and_Company copy.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long
+#define el '\n'
+#define all(arr) arr.begin(), arr.end()
+#define allr(arr) arr.rbegin(), arr.rend()
+#define pi 3.14159265358979323846
+using namespace std;
+void setup()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+  setup();
+  int n, k;
+  cin >> n >> k;
+  vector<pair<int, int>> arr(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i].first >> arr[i].second;
+  }
+  sort(all(arr));
+  ll ans = 0, mx = 0;
+  int l = 0;
+  for (int i = 0; i < n; i++)
+  {
+    mx += arr[i].second;
+    while (arr[i].first - arr[l].first >= k)
+    {
+      mx -= arr[l].second;
+      l++;
+    }
+    ans = max(ans, mx);
+  }
+  cout << ans;
+  return 0;
+}
